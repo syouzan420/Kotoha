42544b7d82832d235ebdc79634d86ac9f357bf31
syouzan420 <syouzanponkikki@yahoo.co.jp>
2019-02-13 22:43:33 +0900

    Add files via upload

--- kotoha.hs
+++ kotoha.hs
@@ -23,21 +23,21 @@ display st
   | otherwise = map (\x -> if x=='%' then '/' else x) st 
 
 evaluate :: Ratio Int -> String -> Ratio Int 
-evaluate ac st = ac + section [] [] [] st
+evaluate ac st = ac + section [] [] 1 [] st
 
-section :: String -> String -> String -> String -> Ratio Int 
-section st1 st2 [] [] = (read st1::Int)%1
-section st1 st2 "*" [] = (read st1::Int)*(read st2::Int)%1
-section st1 st2 "/" [] = (read st1::Int)%(read st2::Int)
-section st1 st2 [] (x:xs)
-  | x>='0' && x<='9' || x=='-' = section (st1 ++ [x]) st2 [] xs
-  | x=='x' || x=='*' = section st1 st2 "*" xs 
-  | x=='/' = section st1 st2 "/" xs 
-  | otherwise = section st1 st2 [] xs
-section st1 st2 fs (x:xs)
-  | (x>='0' && x<='9') || x=='-' = section st1 (st2 ++ [x]) fs xs
-  | fs=="*" && (x=='x' || x=='*') = section (show ((read st1::Int)*(read st2::Int))) [] "*" xs 
-  | fs=="*" && x=='/' = section (show ((read st1::Int)*(read st2::Int))) [] "/" xs 
-  | fs=="/" && (x=='x' || x=='*') = section (show ((read st1::Int)%(read st2::Int))) [] "*" xs 
-  | fs=="/" && x=='/' = section (show ((read st1::Int)%(read st2::Int))) [] "/" xs 
-  | otherwise = section st1 st2 fs xs
+section :: String -> String -> Int -> String -> String -> Ratio Int 
+section st1 st2 i [] [] = (read st1::Int) % i
+section st1 st2 i "*" [] = (read st1::Int)*(read st2::Int) % i
+section st1 st2 i "/" [] = (read st1::Int)%((read st2::Int)*i)
+section st1 st2 i [] (x:xs)
+  | x>='0' && x<='9' || x=='-' = section (st1 ++ [x]) st2 i [] xs
+  | x=='x' || x=='*' = section st1 st2 i "*" xs 
+  | x=='/' = section st1 st2 i "/" xs 
+  | otherwise = section st1 st2 i [] xs
+section st1 st2 i fs (x:xs)
+  | (x>='0' && x<='9') || x=='-' = section st1 (st2 ++ [x]) i fs xs
+  | fs=="*" && (x=='x' || x=='*') = section (show ((read st1::Int)*(read st2::Int))) [] i "*" xs 
+  | fs=="*" && x=='/' = section (show ((read st1::Int)*(read st2::Int))) [] i "/" xs 
+  | fs=="/" && (x=='x' || x=='*') = section st1 [] ((read st2::Int)*i) "*" xs 
+  | fs=="/" && x=='/' = section st1 [] ((read st2::Int)*i) "/" xs 
+  | otherwise = section st1 st2 i fs xs
