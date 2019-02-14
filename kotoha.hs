a703c70624e3e768a0912140ef3aef3a5e4f2053
syouzan420 <syouzanponkikki@yahoo.co.jp>
2019-02-14 12:04:13 +0900

    Add files via upload

--- kotoha.hs
+++ kotoha.hs
@@ -2,7 +2,7 @@ import Data.Ratio
 import Data.List (isSuffixOf)
 import Kotof
 
-type Function = String
+type Function = [Char] 
 data Math = Math Function String Int Int
 
 main :: IO ()
@@ -26,21 +26,21 @@ display st
   | otherwise = map (\x -> if x=='%' then '/' else x) st 
 
 evaluate :: Ratio Int -> String -> Ratio Int 
-evaluate ac st = ac + section [] 1 1 [] st
+evaluate ac st = ac + section (Math [] [] 1 1) st
 
-section :: String -> Int -> Int -> String -> String -> Ratio Int 
-section st n d [] [] = (read st::Int) % d 
-section st n d "*" [] = (read st::Int)*n % d
-section st n d "/" [] = n % ((read st::Int)*d)
-section st n d [] (x:xs)
-  | x>='0' && x<='9' || x=='-' = section (st ++ [x]) n d [] xs
-  | x=='x' || x=='*' = section [] (read st::Int) d "*" xs 
-  | x=='/' = section [] (read st::Int) d "/" xs 
-  | otherwise = section st n d [] xs
-section st n d fs (x:xs)
-  | (x>='0' && x<='9') || x=='-' = section (st ++ [x]) n d fs xs
-  | fs=="*" && (x=='x' || x=='*') = section [] ((read st::Int)*n) d "*" xs 
-  | fs=="*" && x=='/' = section [] ((read st::Int)*n) d "/" xs 
-  | fs=="/" && (x=='x' || x=='*') = section [] n ((read st::Int)*d) "*" xs 
-  | fs=="/" && x=='/' = section [] n ((read st::Int)*d) "/" xs 
-  | otherwise = section st n d fs xs
+section :: Math -> String -> Ratio Int 
+section (Math "" st n d) [] = (read st::Int) % d 
+section (Math "*" st n d) [] = (read st::Int)*n % d
+section (Math "/" st n d) [] = n % ((read st::Int)*d)
+section (Math "" st n d) (x:xs)
+  | x>='0' && x<='9' || x=='-' = section (Math [] (st++[x]) n d) xs
+  | x=='x' || x=='*' = section (Math "*" [] (read st::Int) d) xs 
+  | x=='/' = section (Math "/" [] (read st::Int) d) xs 
+  | otherwise = section (Math [] st n d) xs
+section (Math fs st n d) (x:xs)
+  | (x>='0' && x<='9') || x=='-' = section (Math fs (st++[x]) n d) xs
+  | fs=="*" && (x=='x' || x=='*') = section (Math "*" [] ((read st::Int)*n) d) xs 
+  | fs=="*" && x=='/' = section (Math "/" [] ((read st::Int)*n) d) xs 
+  | fs=="/" && (x=='x' || x=='*') = section (Math "*" [] n ((read st::Int)*d)) xs 
+  | fs=="/" && x=='/' = section (Math "/" [] n ((read st::Int)*d)) xs 
+  | otherwise = section (Math fs st n d) xs
