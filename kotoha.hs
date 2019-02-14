79dff10fa09d55198fe9d59e6a2715811848939c
syouzan420 <syouzanponkikki@yahoo.co.jp>
2019-02-14 14:02:39 +0900

    ことは
    foldlを使つて sectionを書きかへ 新なhelper函數を作つて 見やすくした

--- kotoha.hs
+++ kotoha.hs
@@ -2,8 +2,9 @@ import Data.Ratio
 import Data.List (isSuffixOf)
 import Kotof
 
-type Function = [Char] 
-data Math = Math Function String Int Int
+type Function = String
+type Stack = String
+data Math = Math Function Stack Int Int
 
 main :: IO ()
 main = do
@@ -14,33 +15,34 @@ loop :: IO ()
 loop = do
     l <- getLine
     let ls = words l
-    if l=="exit" then return () 
-                 else do
-                    let result = show (foldl evaluate 0 ls)
-                    putStrLn (display result)
-                    loop
+    if l=="exit"
+      then return () 
+      else do
+         let result = show (foldl (\a s -> a+(mathToRatio (foldl calculate (Math "" "" 1 1) s))) 0 ls)
+         putStrLn (display result)
+         loop
 
 display :: String -> String
 display st
   | isSuffixOf "% 1" st = take (length st - 4) st
   | otherwise = map (\x -> if x=='%' then '/' else x) st 
 
-evaluate :: Ratio Int -> String -> Ratio Int 
-evaluate ac st = ac + section (Math [] [] 1 1) st
+mathToRatio :: Math -> Ratio Int
+mathToRatio (Math "" st n d) = (read st) % d
+mathToRatio (Math "*" st n d) = (read st)*n % d
+mathToRatio (Math "/" st n d) = n % ((read st)*d)
 
-section :: Math -> String -> Ratio Int 
-section (Math "" st n d) [] = (read st::Int) % d 
-section (Math "*" st n d) [] = (read st::Int)*n % d
-section (Math "/" st n d) [] = n % ((read st::Int)*d)
-section (Math "" st n d) (x:xs)
-  | x>='0' && x<='9' || x=='-' = section (Math [] (st++[x]) n d) xs
-  | x=='x' || x=='*' = section (Math "*" [] (read st::Int) d) xs 
-  | x=='/' = section (Math "/" [] (read st::Int) d) xs 
-  | otherwise = section (Math [] st n d) xs
-section (Math fs st n d) (x:xs)
-  | (x>='0' && x<='9') || x=='-' = section (Math fs (st++[x]) n d) xs
-  | fs=="*" && (x=='x' || x=='*') = section (Math "*" [] ((read st::Int)*n) d) xs 
-  | fs=="*" && x=='/' = section (Math "/" [] ((read st::Int)*n) d) xs 
-  | fs=="/" && (x=='x' || x=='*') = section (Math "*" [] n ((read st::Int)*d)) xs 
-  | fs=="/" && x=='/' = section (Math "/" [] n ((read st::Int)*d)) xs 
-  | otherwise = section (Math fs st n d) xs
+calculate :: Math -> Char -> Math
+calculate (Math "" st n d) x
+  | x>='0' && x<='9' || x=='-' = Math "" (st++[x]) n d
+  | x=='x' || x=='*' = Math "*" "" (read st) d 
+  | x=='/' = Math "/" "" (read st) d 
+  | otherwise = Math "" st n d
+calculate (Math fs st n d) x
+  | (x>='0' && x<='9') || x=='-' = Math fs (st++[x]) n d
+  | fs=="*" && (x=='x' || x=='*') = Math "*" "" ((read st)*n) d 
+  | fs=="*" && x=='/' = Math "/" "" ((read st::Int)*n) d 
+  | fs=="/" && (x=='x' || x=='*') = Math "*" "" n ((read st)*d) 
+  | fs=="/" && x=='/' = Math "/" "" n ((read st)*d) 
+  | otherwise = Math fs st n d
+  
