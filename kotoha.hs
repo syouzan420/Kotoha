caf47ad16f1f0dcbfc4f2f42421cf782e9478d89
syouzan420 <syouzanponkikki@yahoo.co.jp>
2019-02-17 00:41:20 +0900

    ことは
    括弧の処理を實装しやうとしたが 今のところ / の後の括弧しか処理できない  
    手続を見直す必要はある

--- kotoha.hs
+++ kotoha.hs
@@ -1,9 +1,9 @@
 import Data.Ratio
 import Data.List (isSuffixOf)
 
-type Function = String; type Stack = String
-type Numerator = Int; type Denominator = Int
-data Math = Math Function Stack Numerator Denominator 
+type Function = Char; type Stack = String
+type Numerator = Int; type Denominator = Int; type Flag = Bool
+data Math = Math [Function] Stack Numerator Denominator Flag  
 
 main :: IO ()
 main = do
@@ -12,18 +12,18 @@ main = do
 
 loop :: IO ()
 loop = do
-    l <- getLine
-    let ls = words l
-    if l=="exit"
-      then return () 
-      else do
-         let result = show (foldl selection 0 ls)
-         putStrLn (display result)
-         loop
+  l <- getLine
+  let ls = words l
+  if l=="exit"
+     then return () 
+     else do
+       let result = show (foldl selection 0 ls)
+       putStrLn (display result)
+       loop
 
 selection :: Ratio Int -> String -> Ratio Int
 selection a s@(x:xs)
-  | (x>='0' && x<='9')||x=='-'||x=='+' =  a + mathToRatio (foldl calculate (Math "" "" 1 1) s)
+  | (x>='0' && x<='9')||x=='-'||x=='+' =  a + mathToRatio (foldl calculate (Math "" "" 1 1 True) s)
   | s=="succ" = ((numerator a)+1) % (denominator a)
   | s=="pred" = ((numerator a)-1) % (denominator a)
   | otherwise = 0 % 1
@@ -34,20 +34,28 @@ display st
   | otherwise = map (\x -> if x=='%' then '/' else x) st 
 
 mathToRatio :: Math -> Ratio Int
-mathToRatio (Math "" st n d) = (read st) % d
-mathToRatio (Math "*" st n d) = (read st)*n % d
-mathToRatio (Math "/" st n d) = n % ((read st)*d)
+mathToRatio (Math [] st n d _) = (read st) % d
+mathToRatio (Math [')'] _ n d _) = n % d
+mathToRatio (Math ['*'] st n d b) = if b then (((read st)*n) % d) else (n % ((read st)*d))
+mathToRatio (Math ['x'] st n d b) = if b then (((read st)*n) % d) else (n % ((read st)*d))
+mathToRatio (Math ['/'] st n d b) = if b then (n % ((read st)*d)) else (((read st)*n) % d)
+mathToRatio (Math ['^'] st n d b) = if b then ((n^(read st)) % d) else (n % (d^(read st)))
 
 calculate :: Math -> Char -> Math
-calculate (Math "" st n d) x
-  | x>='0' && x<='9' || x=='-' = Math "" (st++[x]) n d
-  | x=='x' || x=='*' = Math "*" "" (read st) d 
-  | x=='/' = Math "/" "" (read st) d 
-  | otherwise = Math "" st n d
-calculate (Math fs st n d) x
-  | (x>='0' && x<='9') || x=='-' = Math fs (st++[x]) n d
-  | fs=="*" && (x=='x' || x=='*') = Math "*" "" ((read st)*n) d 
-  | fs=="*" && x=='/' = Math "/" "" ((read st)*n) d 
-  | fs=="/" && (x=='x' || x=='*') = Math "*" "" n ((read st)*d) 
-  | fs=="/" && x=='/' = Math "/" "" n ((read st)*d) 
-  | otherwise = Math fs st n d
+calculate (Math [] st n d _) x
+  | x>='0' && x<='9' || x=='-' = Math [] (st++[x]) n d True
+  | x=='x' || x=='*' || x=='/' || x=='^' = Math [x] "" (read st) d True
+  | otherwise = Math [] st n d True
+calculate (Math fl@(f:fs) st n d b) x
+  | (x>='0' && x<='9') || x=='-' = Math fl (st++[x]) n d b
+  | f=='/' && x=='(' = Math (x:fs) st n d (not b)
+  | x==')' && (((f=='x' || f=='*') && b) || (f=='/' && not b)) = Math (x:fs) "" ((read st)*n) d (not b) 
+  | x==')' && (((f=='x' || f=='*') && not b) || (f=='/' && b)) = Math (x:fs) "" n ((read st)*d) (not b) 
+  | f==')' && (x=='x' || x=='*' || x=='/' || x=='^') = Math (x:fs) "" n d b 
+  | (((f=='x' || f=='*' || f=='(') && b)||(f=='/' && not b)) && (x=='x' || x=='*' || x=='/' || x=='^')
+            = Math (x:fs) "" ((read st)*n) d b 
+  | (((f=='x' || f=='*' || f=='(') && not b)||(f=='/' && b)) && (x=='x' || x=='*' || x=='/' || x=='^')
+            = Math (x:fs) "" n ((read st)*d) b 
+  | f=='^' && b && (x=='x' || x=='*' || x=='/' || x=='^') = Math (x:fs) "" (n^(read st)) d b
+  | f=='^' && not b && (x=='x' || x=='*' || x=='/' || x=='^') = Math (x:fs) "" n (d^(read st)) b
+  | otherwise = Math fl st n d b
