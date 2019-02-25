964a569af37957f162ff69ab669c7766a14e7b18
syouzan420 <syouzanponkikki@yahoo.co.jp>
2019-02-25 18:31:12 +0900

    Add files via upload

--- Conv.hs
+++ Conv.hs
@@ -8,23 +8,32 @@ conv line = (unwords $ map change $ init $ words line)
 change :: String -> String
 change s = case (foldl whatis "NULL" s) of
              "NULL" -> ""
-             "NUM" -> "(+("++s++")) .> "
+             "NUM" -> "(+("++(replace s)++")) .> "
+             "NFUNC" -> "("++(replace s)++") .> "
              "FUNC" -> "("++s++") .> "
              _ -> ""
 
 changeLast :: String -> String
 changeLast s = case (foldl whatis "NULL" s) of
                  "NULL" -> ""
-                 "NUM" -> "(+("++s++"))"
+                 "NUM" -> "(+("++(replace s)++"))"
+                 "NFUNC" -> "("++(replace s)++")"
                  "FUNC" -> "("++s++")"
                  _ -> ""
+
+replace :: String -> String
+replace s = foldr check [] s
+              where check ch acc
+                      | ch=='x' = '*':acc
+                      | ch=='/' = '%':acc
+                      | otherwise = ch:acc
  
 whatis :: String -> Char -> String
 whatis acc ch
   | (ch>='0' && ch<='9') && (acc=="NULL" || acc=="NUM") = "NUM"
-  | (ch=='+' || ch=='*') && (acc=="NULL") = "FUNC"
+  | (ch=='+' || ch=='*') && (acc=="NULL") = "NFUNC"
   | ch=='-' && acc=="NULL" = "NUM"
-  | (ch=='+' || ch=='*') && (acc=="NUM") = "NUM"
+  | (ch=='+' || ch=='*' || ch=='x' || ch=='/' || ch=='.') && (acc=="NUM") = "NUM"
   | otherwise = "FUNC"
 
 initParam :: String -> String
--- Func.hs
+++ Func.hs
@@ -1,5 +1,12 @@
 module Func ((.>)) where
 
+import Data.List (isSuffixOf)
+
 infixr 5 .>
 (.>) :: Functor f => f a -> (a -> b) -> f b
 (.>) = flip fmap
+
+display :: String -> String
+display s
+  | isSuffixOf "% 1" s = take (length s - 4) s
+  | otherwise = map (\x -> if x=='%' then '/' else x) s 
