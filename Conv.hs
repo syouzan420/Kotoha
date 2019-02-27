0ae6833254c9e36b8168d66ff010fc9815c58dd9
syouzan420 <syouzanponkikki@yahoo.co.jp>
2019-02-28 06:23:46 +0900

    Add files via upload

--- Conv.hs
+++ Conv.hs
@@ -1,25 +1,41 @@
 import System.Environment (getArgs)
 
-conv :: String -> String
-conv line = (unwords $ map change $ init $ words line)
-              ++ (changeLast $ last $ words line)
-              ++ " $ " ++ initParam (head (words line))
+conv :: [String] -> String
+conv (x:[]) = (inPar 0 (words x)) ++ " $ " ++ initParam x
+conv (x:y:[]) = (inPar 1 (words x)) ++ (inPar 3 (words y)) ++ " $ " ++ initParam x
+conv (x:xs) = (inPar 1 (words x))
+              ++ (unwords $ map (inPar 2) (init (map words xs)))
+              ++ (inPar 3 (words (last xs)))
+              ++ " $ " ++ initParam x
 
-change :: String -> String
-change s = case (foldl whatis "NULL" s) of
+inPar :: Int -> [String] -> String
+inPar i (x:[]) = change i x
+inPar i ls = (foldl ((++).(change 0)) "" (init ls)) ++" "++ (change i (last ls))
+
+change :: Int -> String -> String
+change i s = case (foldl whatis "NULL" s) of
              "NULL" -> ""
-             "NUM" -> "(+("++(replace s)++")) .> "
-             "NFUNC" -> "("++(replace s)++") .> "
-             "FUNC" -> "("++s++") .> "
+             "NUM" -> if (i==1 || i==2) then "(+("++(replace s)++")) .> "
+                                        else "(+("++(replace s)++"))"
+             "NFUNC" -> if (i==1 || i==2) then "("++[(head (replace s))]++"("++(tail (replace s))++")) .> "
+                                          else "("++[(head (replace s))]++"("++(tail (replace s))++"))"
+             "LIST" -> case i of
+                          0 -> "toList .> (++ "++s++" ) .> tail"
+                          1 -> "toList .> (++ "++s++" ).> tail .> "
+                          2 -> s++" .>"
+                          3 -> s 
+             "FUNC" -> if (i==1 || i==2) then s++" .> "
+                                         else s++" "
              _ -> ""
 
-changeLast :: String -> String
-changeLast s = case (foldl whatis "NULL" s) of
-                 "NULL" -> ""
-                 "NUM" -> "(+("++(replace s)++"))"
-                 "NFUNC" -> "("++(replace s)++")"
-                 "FUNC" -> "("++s++")"
-                 _ -> ""
+joinPar :: String -> Bool -> [String] -> [String]
+joinPar _ _ [] = []
+joinPar s False (x:xs)
+  | head x == '(' = joinPar (tail x) True xs
+  | otherwise = x : (joinPar s False xs)
+joinPar s True (x:xs)
+  | last x == ')' = (s++" "++(init x)) : (joinPar "" False xs)
+  | otherwise = joinPar (s++" "++x) True xs
 
 replace :: String -> String
 replace s = foldr check [] s
@@ -30,11 +46,15 @@ replace s = foldr check [] s
  
 whatis :: String -> Char -> String
 whatis acc ch
-  | (ch>='0' && ch<='9') && (acc=="NULL" || acc=="NUM") = "NUM"
-  | (ch=='+' || ch=='*') && (acc=="NULL") = "NFUNC"
+  | (ch>='0' && ch<='9') && acc=="NULL" = "NUM"
+  | (ch>='0' && ch<='9') && acc=="NUM" = "NUM"
+  | (ch=='+' || ch=='*') && acc=="NULL" = "NFUNC"
+  | ch=='[' && acc=="NULL" = "LIST_"
+  | ch==']' && acc=="LIST_" = "LIST"
   | ch=='-' && acc=="NULL" = "NUM"
-  | (ch=='+' || ch=='*' || ch=='x' || ch=='/' || ch=='.') && (acc=="NUM") = "NUM"
-  | otherwise = "FUNC"
+  | (ch>='a' && ch<='x') && acc=="NULL" = "FUNC"
+  | (ch=='+' || ch=='*' || ch=='x' || ch=='/' || ch=='.') && acc=="NUM" = "NUM"
+  | otherwise = acc 
 
 initParam :: String -> String
 initParam _ = "0"
@@ -43,5 +63,5 @@ main :: IO ()
 main = do
   args <- getArgs
   let exp = head args
-  print (conv exp)
+  print (conv (joinPar "" False (words exp)))
 
